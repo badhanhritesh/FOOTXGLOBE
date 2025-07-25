c7481afec6d1feceedf80193162c4ecb3a86f636
HRITESH BADHAN <109867387+badhanhritesh@users.noreply.github.com>
2025-07-25 09:49:51 +0530

    Add files via upload

---
+++ FootFutBallStoreApplication.java
@@ -0,0 +1,15 @@
+package com.football.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootFutBallStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootFutBallStoreApplication.class, args);
+	}
+
+}
+//WEBSITE NAMES:
+//1: FUTXGLOBE
